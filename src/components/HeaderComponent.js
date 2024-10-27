5837de2dcc8ff0c4c167e32ff83b703f84b3f316
zobirofkir <zobirofkir19@gmail.com>
2024-10-27 20:44:00 +0100

    make logo href

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -10,12 +10,14 @@ const HeaderComponent = () => {
   return (
     <div className='bg-white px-8'>
       <div className='flex justify-between items-center w-full py-5'>
-        {/* Logo Section */}
+
         <div className='flex items-center'>
-          <h1 className='text-black font-bold md:text-xl text-md'>Prodmast</h1>
+          <a href='#home'>
+            <h1 className='text-black font-bold md:text-xl text-md'>Prodmast</h1>
+          </a>
         </div>
 
-        {/* Hamburger Icon for Mobile */}
+
         <div className='md:hidden flex items-center'>
           <button onClick={toggleMenu} className='text-black focus:outline-none'>
             <svg className='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
@@ -24,25 +26,25 @@ const HeaderComponent = () => {
           </button>
         </div>
 
-        {/* Navigation Links - Desktop */}
+
         <div className='hidden md:flex items-center'>
           <ul className='flex justify-between gap-4'>
-            <a href='/'>
+            <a href='#home'>
               <li className='text-black font-bold md:text-md text-sm'>Home</li>
             </a>
-            <a href='/abouts'>
+            <a href='#about'>
               <li className='text-black font-bold md:text-md text-sm'>About</li>
             </a>
-            <a href='/services'>
+            <a href='#services'>
               <li className='text-black font-bold md:text-md text-sm'>Services</li>
             </a>
-            <a href='/contacts'>
+            <a href='#contacts'>
               <li className='text-black font-bold md:text-md text-sm'>Contacts</li>
             </a>
           </ul>
         </div>
 
-        {/* Get Started Button - Desktop */}
+
         <div className='hidden md:flex items-center'>
           <button className='bg-green-950 text-white font-bold md:text-md text-sm px-4 py-3 rounded-md'>Get Started</button>
         </div>
@@ -55,16 +57,16 @@ const HeaderComponent = () => {
       >
         <div className='md:hidden t'>
           <ul className='flex flex-col gap-4'>
-            <a href='/'>
+            <a href='#home'>
               <li className='text-black font-bold md:text-md text-sm'>Home</li>
             </a>
-            <a href='/abouts'>
+            <a href='#about'>
               <li className='text-black font-bold md:text-md text-sm'>About</li>
             </a>
-            <a href='/services'>
+            <a href='#services'>
               <li className='text-black font-bold md:text-md text-sm'>Services</li>
             </a>
-            <a href='/contacts'>
+            <a href='#contacts'>
               <li className='text-black font-bold md:text-md text-sm'>Contacts</li>
             </a>
             <button className='bg-green-950 text-white font-bold md:text-md text-sm px-4 py-3 rounded-md my-4'>
