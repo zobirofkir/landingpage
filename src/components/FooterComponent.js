431eb04b044c39df4fa86ee868ccaca65c36315d
zobirofkir <zobirofkir19@gmail.com>
2024-10-28 07:28:06 +0100

    new deploy

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div className='flex justify-center items-center p-10'>
+        <p>Copyright &copy; {new Date().getFullYear()} - {new Date().getFullYear() + 1} - By - Zobir Ofkir</p>
+    </div>
+  )
+}
+
+export default FooterComponent 
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,6 +4,7 @@ import SliderComponent from '../components/SliderComponent'
 import ServiceComponent from '../components/ServiceComponent'
 import AboutComponent from '../components/AboutComponent'
 import ContactComponent from '../components/ContactComponent'
+import FooterComponent from '../components/FooterComponent'
 
 const HomeScreen = () => {
   return (
@@ -13,6 +14,7 @@ const HomeScreen = () => {
         <AboutComponent/>
         <ServiceComponent/>
         <ContactComponent/>
+        <FooterComponent/>
     </div>
   )
 }
